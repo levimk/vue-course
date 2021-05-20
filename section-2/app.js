5616ae28951292f6100119b6680f73c8b5a7c7fd
Levi <levimckenzie93@gmail.com>
2021-05-20 19:06:43 +1000

    challenge: add to faves

--- section-2/app.js
+++ section-2/app.js
@@ -8,8 +8,8 @@ const app = Vue.createApp({
             x: 0,
             y: 0,
             books: [
-                {id: 1, title: "The Catcher in the Rye", author: "J.D. Salinger", likes: 0, url: "https://en.wikipedia.org/wiki/The_Catcher_in_the_Rye", img: "assets/salinger.jpg"},
-                {id: 2, title: "Conjectures and Refutations", author: "Karl Popper", likes: 0, url: "https://www.routledge.com/Conjectures-and-Refutations-The-Growth-of-Scientific-Knowledge/Popper/p/book/9780415285940#:~:text=Conjectures%20and%20Refutations%20is%20one,to%20politics%20and%20to%20history.", img: "assets/popper.jpg"},
+                {id: 1, title: "The Catcher in the Rye", author: "J.D. Salinger", likes: 0, url: "https://en.wikipedia.org/wiki/The_Catcher_in_the_Rye", img: "assets/salinger.jpg", isFav: false},
+                {id: 2, title: "Conjectures and Refutations", author: "Karl Popper", likes: 0, url: "https://www.routledge.com/Conjectures-and-Refutations-The-Growth-of-Scientific-Knowledge/Popper/p/book/9780415285940#:~:text=Conjectures%20and%20Refutations%20is%20one,to%20politics%20and%20to%20history.", img: "assets/popper.jpg", isFav: false},
                 {id: 3, title: "Maps of Meaning", author: "Jordan B. Peterson", likes: 0, url: "https://www.jordanbpeterson.com/maps-of-meaning/", img: "assets/peterson.jpg", isFav: true}
             ]
         }
@@ -31,6 +31,9 @@ const app = Vue.createApp({
         handleMousemove(e) {
             this.x = e.offsetX
             this.y = e.offsetY
+        },
+        toggleFav(book) {
+            book.isFav = !book.isFav;
         }
     }
 })
