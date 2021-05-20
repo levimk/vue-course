45640f48f9ed0cd4ebc6a21cf7acf64b403cde40
Levi <levimckenzie93@gmail.com>
2021-05-20 18:47:35 +1000

    lesson: dynamic classes

--- section-2/app.js
+++ section-2/app.js
@@ -10,7 +10,7 @@ const app = Vue.createApp({
             books: [
                 {id: 1, title: "The Catcher in the Rye", author: "J.D. Salinger", likes: 0, url: "https://en.wikipedia.org/wiki/The_Catcher_in_the_Rye", img: "assets/salinger.jpg"},
                 {id: 2, title: "Conjectures and Refutations", author: "Karl Popper", likes: 0, url: "https://www.routledge.com/Conjectures-and-Refutations-The-Growth-of-Scientific-Knowledge/Popper/p/book/9780415285940#:~:text=Conjectures%20and%20Refutations%20is%20one,to%20politics%20and%20to%20history.", img: "assets/popper.jpg"},
-                {id: 3, title: "Maps of Meaning", author: "Jordan B. Peterson", likes: 0, url: "https://www.jordanbpeterson.com/maps-of-meaning/", img: "assets/peterson.jpg"}
+                {id: 3, title: "Maps of Meaning", author: "Jordan B. Peterson", likes: 0, url: "https://www.jordanbpeterson.com/maps-of-meaning/", img: "assets/peterson.jpg", isFav: true}
             ]
         }
     },
