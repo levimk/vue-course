02a534201e71961a6deb4eb26f73abd793d06343
Levi <levimckenzie93@gmail.com>
2021-05-20 18:19:51 +1000

    attribute binding

--- section-2/app.js
+++ section-2/app.js
@@ -8,9 +8,9 @@ const app = Vue.createApp({
             x: 0,
             y: 0,
             books: [
-                {id: 1, title: "The Catcher in the Rye", authro: "J.D. Salinger", likes: 0},
-                {id: 2, title: "Conjectures and Refutations", authro: "Karl Popper", likes: 0},
-                {id: 3, title: "Maps of Meaning", authro: "Jordan B. Peterson", likes: 0}
+                {id: 1, title: "The Catcher in the Rye", author: "J.D. Salinger", likes: 0, url: "https://en.wikipedia.org/wiki/The_Catcher_in_the_Rye", img: "assets/salinger.jpg"},
+                {id: 2, title: "Conjectures and Refutations", author: "Karl Popper", likes: 0, url: "https://www.routledge.com/Conjectures-and-Refutations-The-Growth-of-Scientific-Knowledge/Popper/p/book/9780415285940#:~:text=Conjectures%20and%20Refutations%20is%20one,to%20politics%20and%20to%20history.", img: "assets/popper.jpg"},
+                {id: 3, title: "Maps of Meaning", author: "Jordan B. Peterson", likes: 0, url: "https://www.jordanbpeterson.com/maps-of-meaning/", img: "assets/peterson.jpg"}
             ]
         }
     },
