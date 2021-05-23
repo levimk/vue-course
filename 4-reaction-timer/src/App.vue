f762c401d9005b2683e3b1dda622b275c91f7e87
Levi <levimckenzie93@gmail.com>
2021-05-23 15:43:58 +1000

    :construction: init section 4 - reaction timer

---
+++ 4-reaction-timer/src/App.vue
@@ -0,0 +1,81 @@
+<template>
+  <h1>Ninja Reaction Timer</h1>
+  <div class="buttons">
+    <button class="start" @click="start" :disabled='isPlaying'>Start game</button>
+  </div>
+  <Block v-if='isPlaying' message="Click me!" :delay='delay'/>
+</template>
+
+<script>
+import Block from './components/Block'
+
+export default {
+  name: 'App',
+  components: {
+    Block
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true
+      console.log("Delay: ", this.delay)
+    }
+  },
+  data() {
+    return {
+      isPlaying: false,
+      delay: 0
+    }
+  }
+}
+</script>
+
+<style>
+/*
+  turqois: rgb(43, 212, 161)
+*/
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin: 60px auto 60px auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+}
+
+button {
+  background: transparent;
+  border: 1px solid rgb(43, 212, 161);
+  border-radius: 5px;
+  padding: 5px;
+  color: rgb(43, 212, 161);
+}
+
+button:hover {
+  border-width: 2px;
+  font-weight: bold;
+  cursor: pointer;
+}
+
+button:disabled {
+  border-color: #ddd;
+  color: #ddd;
+}
+
+button:hover:disabled {
+  border-width: 1px;
+  font-weight: normal;
+  cursor: pointer;
+}
+
+.buttons {
+  width: 30%;
+  max-width: 300px;
+  display: flex;
+  justify-content: space-between;
+}
+</style>
---
+++ 4-reaction-timer/src/components/Block.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="block">
+    <p>{{ message }}</p>
+    <p>{{ delay }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['message', 'delay']
+}
+</script>
+
+<style>
+.block {
+    width: 400px;
+    border-radius: 10px;
+    background: rgb(43, 212, 161);
+    text-align: center;
+    padding: 100px 0;
+    margin: 40px auto;
+    color: white;
+    font-weight: 600;
+}
+</style>
