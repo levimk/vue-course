b4053cb4da529df75bf4ac77f39b7620098c4210
Levi <levimckenzie93@gmail.com>
2021-07-26 10:08:35 +1000

    Init app: muso ninjas

---
+++ muso-ninjas/src/composables/getCollection.js
@@ -0,0 +1,38 @@
+import { ref, watchEffect } from 'vue';
+import { projectFirestore } from '../firebase/config';
+
+const getCollection = (collection) => {
+  const documents = ref(null);
+  const error = ref(null);
+
+  // register the firestore collection reference
+  let collectionRef = projectFirestore
+    .collection(collection)
+    .orderBy('createdAt');
+
+  const unsubscribe = collectionRef.onSnapshot(
+    (snap) => {
+      let results = [];
+      snap.docs.forEach((doc) => {
+        // must wait for the server to create the timestamp & send it back
+        // we don't want to edit data until it has done this        console.log(`${doc.id}: ${doc.data().name}: ${doc.data().message}`);
+        doc.data().createdAt && results.push({ ...doc.data(), id: doc.id });
+      });
+
+      // update values
+      documents.value = results;
+      error.value = null;
+    },
+    (err) => {
+      console.log(err.message);
+      documents.value = null;
+      error.value = 'could not fetch the data';
+    }
+  );
+
+  watchEffect((onInvalidate) => onInvalidate(() => unsubscribe()));
+
+  return { error, documents };
+};
+
+export default getCollection;
---
+++ muso-ninjas/src/firebase/config.js
@@ -0,0 +1,25 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyCDTkYK7JSABefkGPABgfTfZbDWjHXvZJw',
+  authDomain: 'muso-ninjas-2bc5f.firebaseapp.com',
+  projectId: 'muso-ninjas-2bc5f',
+  storageBucket: 'muso-ninjas-2bc5f.appspot.com',
+  messagingSenderId: '652448939021',
+  appId: '1:652448939021:web:c301ac9f01ff513523a944',
+  measurementId: 'G-ZX2862FLQE',
+};
+
+firebase.initializeApp(firebaseConfig);
+
+// init services
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+// timestamp
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { projectFirestore, projectAuth, timestamp };
