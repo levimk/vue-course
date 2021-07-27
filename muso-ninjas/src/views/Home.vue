180b7e39658429cafc6a0574225c185e0b103df4
Levi <levimckenzie93@gmail.com>
2021-07-28 09:34:54 +1000

    List of playlists

--- muso-ninjas/src/views/Home.vue
+++ muso-ninjas/src/views/Home.vue
@@ -2,11 +2,39 @@
   <div class="home">
     <h2>Home</h2>
   </div>
+  <div v-if="error" class="error">
+    There was an error loading the playlists.
+  </div>
+  <ListView :playlists="documents" />
 </template>
 
 <script>
-// @ is an alias to /src
+import getCollection from '@/composables/getCollection'
+import ListView from '@/components/ListView'
+
 export default {
-  
+  components: {
+    ListView
+  },
+  setup() {
+    const { error, documents } = getCollection('playlists')
+
+    return { error, documents }
+  }
 }
 </script>
+
+<style scoped>
+p.playlist-author {
+  color: lightslategray;
+  font-weight: lighter;
+}
+
+ul#playlistList {
+  margin-top: 10px;
+  list-style-type: none;
+}
+ul#playlistList li {
+  margin-bottom: 15px;
+}
+</style>>
